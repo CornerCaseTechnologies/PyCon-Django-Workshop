3f2d3d53f17cf5822de85187ce7eb7f2e5e8bc13
Gytis <petra.gytis@gmail.com>
2023-05-07 10:15:34 +0000

    More date field changes

--- company/models.py
+++ company/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -21,7 +21,7 @@ class Employee(models.Model):
     email = models.EmailField(max_length=128)
     position = models.CharField(max_length=64, choices=POSITIONS)
     experience = models.IntegerField(validators=[MinValueValidator(0)], help_text="Number of years working in the company")
-    date_of_birth = models.DateField(default=datetime.today().date())
+    date_of_birth = models.DateField(default=date.today)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -38,8 +38,8 @@ class Room(models.Model):
 
 
 class Reservation(models.Model):
-    reserved_from = models.DateTimeField(default=datetime.now())
-    reserved_to = models.DateTimeField(default=datetime.now())
+    reserved_from = models.DateTimeField(default=datetime.now)
+    reserved_to = models.DateTimeField(default=datetime.now)
     room = models.ForeignKey(Room, related_name="reservations", on_delete=models.CASCADE)
     host = models.ForeignKey(Employee, related_name="hosted_reservations", null=True, blank=True, on_delete=models.SET_NULL)
     attendees = models.ManyToManyField(Employee, related_name="attended_reservations")
