b2a70f42ec0d840a71fb4de0366fc96c47b3d861
Gytis Petrauskas <petra.gytis@gmail.com>
2023-05-09 14:36:53 +0000

    Filter task

---
+++ company/filters.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from django_filters import FilterSet, NumberFilter, BooleanFilter, CharFilter, ChoiceFilter
+from django.db.models import Q
+from django.db.models.query import QuerySet
+
+from company.models import Employee, Reservation, Room
+
+
+class RoomFilter(FilterSet):
+    min_capacity = NumberFilter(field_name="capacity", lookup_expr="gte")
+    is_reserved = BooleanFilter(method="filter_reserved", label="Is Reserved")
+
+    class Meta:
+        model = Room
+        fields = ["min_capacity", "is_reserved"]
+
+    def filter_reserved(self, queryset: QuerySet[Room], name: str, value: bool | None) -> QuerySet[Room]:
+        now = datetime.now()
+        reserved_rooms = queryset.filter(reservations__reserved_from__lte=now, reservations__reserved_to__gt=now)
+        if value:
+            return reserved_rooms
+        elif value == None:
+            return queryset
+        elif not value:
+            return queryset.exclude(pk__in=reserved_rooms.values_list("pk", flat=True))
+        
+
+class EmployeeFilter(FilterSet):
+    name = CharFilter(method="filter_name", label="Name")
+    max_experience = NumberFilter(field_name="experience", lookup_expr="lte")
+    assumed_position = ChoiceFilter(choices=Employee.POSITIONS)
+
+    class Meta:
+        model = Employee
+        fields = ["name", "max_experience", "assumed_position"]
+
+    def filter_name(self, queryset: QuerySet[Employee], name: str, value: str) -> QuerySet[Employee]:
+        return queryset.filter(Q(first_name__icontains=value) | Q(last_name__icontains=value))
+
+
+class ReservationFilter(FilterSet):
+    room_id = NumberFilter(field_name="room__id")
+
+    class Meta:
+        model = Reservation
+        fields = ["room_id"]
--- company/views.py
+++ company/views.py
@@ -1,7 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
+from company.filters import EmployeeFilter, ReservationFilter, RoomFilter
 
 from company.models import Employee, Reservation, Room
 from company.serializers import AttendeeAddSerializer, EmployeeSerializer, ReservationSerializer, RoomSerializer
@@ -10,25 +13,32 @@ from company.serializers import AttendeeAddSerializer, EmployeeSerializer, Reser
 class EmployeeViewSet(ModelViewSet):
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = EmployeeFilter
 
 
 class RoomViewSet(ModelViewSet):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = RoomFilter
+
 
 
 class ReservationViewSet(ModelViewSet):
     serializer_class = ReservationSerializer
     queryset = Reservation.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = ReservationFilter
 
-    @action(detail=True, methods=['PUT'], serializer_class=AttendeeAddSerializer)
+    @action(detail=True, methods=["PUT"], serializer_class=AttendeeAddSerializer)
     def add_attendee(self, request: Request, pk: int | None = None):
         reservation = self.get_object()
         serializer = self.get_serializer(
             reservation, 
             data=request.data, 
             partial=True,
-            context={'reservation': reservation}
+            context={"reservation": reservation}
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
