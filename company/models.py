79eea0fbc90f4b9e7050c24f30b20fba623cf13a
Gytis Petrauskas <petra.gytis@gmail.com>
2023-05-12 13:46:17 +0000

    Add enums

--- company/models.py
+++ company/models.py
@@ -4,22 +4,14 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 from api.settings import YEAR_IN_DAYS
+from company import Positions
 
 
 class Employee(models.Model):
-    POSITIONS = [
-        ("manager", "Manager"),
-        ("senior_developer", "Senior Developer"),
-        ("developer", "Developer"),
-        ("junior_developer", "Junior Developer"),
-        ("designer", "Designer"),
-        ("tester", "Tester"),
-    ]
-
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField(max_length=128)
-    position = models.CharField(max_length=64, choices=POSITIONS)
+    position = models.CharField(max_length=64, choices=Positions.get_choices())
     experience = models.IntegerField(
         validators=[MinValueValidator(0)],
         help_text="Number of years working in the company",
